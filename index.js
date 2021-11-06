111d4a4d9e65e56d0ce0af3479cf2f507182aa27
elout-project <hidayat.mudha@gmail.com>
2021-11-06 17:20:46 +0700

    add sendFile

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/about',(req,res)=>{
     res.send('His is my first deployment to heroku, very statisfiying . Thank heroku')
 })
 app.get('/',(req,res)=>{
-    res.send("https://elout-project.heroku.com/srt-resume.html")
+    res.sendFile("https://elout-project.heroku.com/srt-resume.html")
 })
 
 app.use('/',(req,res)=>{
