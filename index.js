ca8773a85f7da0a9437d02ad9833210c24edfacc
elout-project <hidayat.mudha@gmail.com>
2021-11-06 16:38:20 +0700

    adding 404

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ app.get('/',(req,res)=>{
     res.send('<h2>Hello world, sugeng sonten !</h2>')
 })
 
+app.use('/',(req,res)=>{
+    res.send('404 Error, Not found')
+})
+
 app.listen(PORT,()=>console.log('Server running http://localhost:3300'))
