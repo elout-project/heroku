02d198d86b123bf54110e859bf26f46e952240da
elout-project <hidayat.mudha@gmail.com>
2021-11-06 16:36:20 +0700

    Adding /about routes'

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const PORT = process.env.PORT || 3300
 const app = express()
+
+app.get('/about',(req,res)=>{
+    res.send('His is my first deployment to heroku, very statisfiying . Thank heroku')
+})
 app.get('/',(req,res)=>{
     res.send('<h2>Hello world, sugeng sonten !</h2>')
 })
